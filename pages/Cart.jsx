6d5193bd4e8332d5ad3aa904237392876ea6c66a
unknown <mnyrskyk@gmail.com>
2024-06-23 03:28:19 -0800

    card upgrades

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import CartContext from '../contexts/cartContext';
 
+import { FaTrash } from 'react-icons/fa'; // Font Awesome
+import { MdDelete } from 'react-icons/md'; // Material Design
+
+
+
 function Cart() {
   const { cartItems, setCartItems } = React.useContext(CartContext);
 
@@ -11,7 +16,12 @@ function Cart() {
           <div key={index} className='cart-item'>
             <h2 className='cart-item-title'>{item.optionName}</h2>
             <p className='cart-item-catego'>Category: {item.collectionName}</p>
-            <h3 className='mo'>momomomoomomo</h3>
+            <h3 className='cart-delete-button'><FaTrash/></h3>
+            <select className='cart-choices'>
+              <option value='12months'>12 months</option>
+              <option value='3months'>3 months</option>
+            </select>
+            <h4 className='cart-item-price'>price: 1250$</h4>
           </div>
         ))
       ) : (
