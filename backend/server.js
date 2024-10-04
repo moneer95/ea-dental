955100935efc7c97888c41162f17981f1941f69d
Monir Skaik <monirskaik@192.168.1.2>
2024-10-04 19:15:17 +0300

    too close

--- backend/server.js
+++ backend/server.js
@@ -179,9 +179,8 @@ app.get('/session-status', async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
   console.log(session.status)
 
-  const arrays = await getClientArrays(session.id)
+  const { products, tickets, courses, bookings, weight } = await getClientArrays(session.id)
   console.log(session.id + ' -----  clientttt' )
-  console.log(arrays)
 
 
   if (session.payment_status === 'paid') {    
