2e4786477a19383d35d62530fb1bd229f12c40a1
Monir Skaik <monirskaik@Monirs-MacBook-Air.local>
2024-10-17 14:06:33 +0300

    too close

--- backend/discounts.js
+++ backend/discounts.js
@@ -1,22 +1,25 @@
 
-
-function getDicountFromCompination(tickets) {
+function getDicountFromCompination(tickets){
     let option1 = 'ORE2 Comprehensive Theory Course';
     let option2 = 'ORE2: Five-Day Comprehensive Online Course + One In-Person Day';
     let option3 = '3-Day ORE2 Manikin Mastery';
-
-    let hasBothOptions = (
-        tickets.some(ticket => ticket.optionName && (ticket.optionName.trim() == option1 || ticket.optionName.trim() == option2))
-    ) && tickets.some(ticket => ticket.optionName && ticket.optionName.trim() == option3);
+    
+    let hasBothOptions = (tickets.some(ticket => ticket.optionName == option1) || tickets.some(ticket => ticket.optionName == option2)) &&
+                     tickets.some(ticket => ticket.optionName == option3);
 
     if (hasBothOptions) {
         console.log('Both option names exist in the tickets array');
         return 1;
     } else {
         console.log('One or both option names are missing');
+        console.log(tickets.some(ticket => {console.log(ticket.optionName)}))
+        console.log(tickets.some(ticket => {console.log(typeof(ticket.optionName))}))
+        console.log(tickets.some(ticket => ticket.optionName == option3))
         return 0;
     }
+    
 }
+
 
 module.exports = {
     getDicountFromCompination
