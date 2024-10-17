31933021ee74b07f0efddd50c9f591812ca7a42d
Monir Skaik <monirskaik@Monirs-MacBook-Air.local>
2024-10-17 14:38:10 +0300

    too close

--- backend/discounts.js
+++ backend/discounts.js
@@ -11,7 +11,7 @@ function getDicountFromCompination(tickets){
 
 
     console.log('------------')                 
-    console.log(tickets)
+    console.log(typeof(tickets))
 
     if (hasBothOptions) {
         console.log('Both option names exist in the tickets array');
